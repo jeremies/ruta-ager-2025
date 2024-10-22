b41400b6822c83de4e3fa6c1f5f6a99b56250750
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2024-10-23 00:00:59 +0200

    feat: musica with react-player

--- src/Musica.jsx
+++ src/Musica.jsx
@@ -1,24 +1,56 @@
 import * as React from "react";
-import Typography from "@mui/material/Typography";
-import Iframe from "./Iframe";
+import ReactPlayer from "react-player/lazy";
 
+import Box from "@mui/material/Box";
+import FormControl from "@mui/material/FormControl";
+import NativeSelect from "@mui/material/NativeSelect";
+
+const media = [
+  { label: "Sel·lecciona una cançó", url: "" },
+  {
+    label: "«La Felicidad», Izaro",
+    url: "https://www.youtube.com/watch?v=rzp5foKF9Ps",
+  },
+  {
+    label: "Sálvame, Íñigo Quintero",
+    url: "https://www.youtube.com/watch?v=6UAinV-h6VI",
+  },
+  {
+    label: "«Sé que no estic sol», Worship.cat",
+    url: "https://www.youtube.com/watch?v=HdV20_4pdDo",
+  },
+  {
+    label: "«Al salir al camino», Rezando voy",
+    url: "https://rezando.es/public/rezandovoy/oraciones/2559/o.mp3",
+  },
+];
 export default function Musica() {
+  const [url, setUrl] = React.useState(() => media[0]);
   return (
     <div>
-      <Typography variant="h5" gutterBottom>
-        «La Felicidad», Izaro
-      </Typography>
-      <Iframe src="https://www.youtube.com/embed/rzp5foKF9Ps?si=cjNCefGdubEb1Cgg" />
-      <Typography variant="h5" gutterBottom>
-        Sálvame, Íñigo Quintero
-      </Typography>
-      <Iframe src="https://www.youtube.com/embed/6UAinV-h6VI?si=m2cGsK1G4yZoiTg_" />
-      <Typography variant="h5" gutterBottom>
-        «Sé que no estic sol», Worship.cat{" "}
-      </Typography>
-      <Iframe src="https://www.youtube.com/embed/HdV20_4pdDo?si=IL0xGCUFkVLPVsAZ" />
-      TODOjmc separar Podcasts
-      <Iframe src="https://rezandovoy.org/reproductor/especial/2559-camino-de-santiago-01---al-salir-al-camino" />
+      <Box sx={{ minWidth: 120 }}>
+        <FormControl fullWidth>
+          <NativeSelect
+            inputProps={{
+              name: "media",
+              id: "uncontrolled-native",
+            }}
+            onChange={(e) => setUrl(e.target.value)}
+          >
+            {media.map((med) => (
+              <option value={med.url}>{med.label}</option>
+            ))}
+          </NativeSelect>
+        </FormControl>
+      </Box>
+
+      <ReactPlayer
+        style={{ marginTop: "30px" }}
+        width="80vw"
+        height="calc(2 * (80vw) /3)"
+        controls
+        url={url}
+      />
     </div>
   );
 }
