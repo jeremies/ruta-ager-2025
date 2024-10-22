784e7cf535bc4b9a1c019b29096b47084f9e1d7d
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2024-10-23 00:23:40 +0200

    treure login

--- src/AppBar.jsx
+++ src/AppBar.jsx
@@ -12,19 +12,9 @@ export default function AppBar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBarMui position="fixed">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Ruta del cister 2024
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBarMui>
     </Box>
