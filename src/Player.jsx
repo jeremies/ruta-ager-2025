7d31e0f49231c9cb39e01d051068d86ab8395899
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2024-10-26 18:20:46 +0200

    feat: add logo EJL

--- src/Player.jsx
+++ src/Player.jsx
@@ -5,7 +5,8 @@ import Box from "@mui/material/Box";
 import FormControl from "@mui/material/FormControl";
 import NativeSelect from "@mui/material/NativeSelect";
 import { Typography } from "@mui/material";
-
+import Logo from "./logo.svg?react";
+import "./Player.css";
 export default function Player({ title, media }) {
   const [url, setUrl] = React.useState(media[0].url);
   return (
@@ -31,13 +32,32 @@ export default function Player({ title, media }) {
         </FormControl>
       </Box>
 
-      <ReactPlayer
-        style={{ marginTop: "30px" }}
-        width="80vw"
-        height="calc(2 * (80vw) /3)"
-        controls
-        url={url}
-      />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <ReactPlayer
+          style={{ marginTop: "30px" }}
+          width="80vw"
+          height={url.includes("youtube") ? "calc(2 * (80vw) /3)" : "40px"}
+          controls
+          url={url}
+        />
+      </Box>
+
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          marginTop: 2,
+        }}
+      >
+        <Logo className="logo" />
+      </Box>
     </div>
   );
 }
