c871707759810a47b54ed51460a94292176eb849
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2024-10-26 20:33:38 +0200

    feat: ruta dia 3

--- src/Ruta.jsx
+++ src/Ruta.jsx
@@ -9,24 +9,41 @@ export default function Ruta({ day }) {
       </Typography>
       {day == 1 && (
         <>
-          <Typography variant="h5" gutterBottom>
+          <Typography variant="h5" sx={{ marginBottom: 3 }} gutterBottom>
             - Fes memòria dels moments més feliços de la teva vida
           </Typography>
-          <Typography variant="h5" gutterBottom>
+          <Typography variant="h5" sx={{ marginBottom: 3 }} gutterBottom>
             - Comparteix-ho amb la teva parella de recés
           </Typography>
         </>
       )}
       {day == 2 && (
         <>
-          <Typography variant="h5" gutterBottom>
+          <Typography variant="h5" sx={{ marginBottom: 3 }} gutterBottom>
             - Havent sentit les paraules de Jesús: t’has sentit identificat?
             t’has sentit interpel·lat? desperten alguna cosa en tu?
           </Typography>
-          <Typography variant="h5" gutterBottom>
+          <Typography variant="h5" sx={{ marginBottom: 3 }} gutterBottom>
             - Per parelles: qui és Jesús per a tu? què signifiquen per a tu les
             benaurances del Senyor? Compartiu les vostres respostes amb la
             parella del recés.
+          </Typography>
+        </>
+      )}
+      {day == 3 && (
+        <>
+          <Typography variant="h5" sx={{ marginBottom: 3 }} gutterBottom>
+            - Si la vida es pot definir com «un camí de retorn»: com et
+            relaciones amb el Senyor? Experimentes que aquest és el camí de la
+            felicitat? Com ho vius? Has de canviar alguna cosa? Has d’afrontar
+            algun interrogant? Com et tracta el Senyor? Com el tractes tu?
+          </Typography>
+          <Typography variant="h5" sx={{ marginBottom: 3 }} gutterBottom>
+            - Què és el que et qüestiona més d’aquesta paràbola? Per què?
+          </Typography>
+          <Typography variant="h5" sx={{ marginBottom: 3 }} gutterBottom>
+            - Comparteix les teves respostes amb la parella del recés. De
+            baixada per la pista.
           </Typography>
         </>
       )}
