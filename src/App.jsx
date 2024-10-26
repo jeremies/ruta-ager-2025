8e3745314bb2a6402e21a79d4305b4ffe497d773
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2024-10-26 18:44:09 +0200

    feat: colors corporatius

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
 import PWABadge from "./PWABadge.jsx";
 import "./App.css";
 import Paper from "./Paper.jsx";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#701147",
+    },
+  },
+});
+
 function App() {
   return (
     <>
-      <Router>
-        <Paper />
-      </Router>
-      <PWABadge />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Paper />
+        </Router>
+        <PWABadge />
+      </ThemeProvider>
     </>
   );
 }
