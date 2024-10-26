6a927e753c6d1099bf19c09f51a9279c1c9a253b
Joaquim Monserrat Companys <jmonserrat@protonmail.com>
2024-10-26 16:44:02 +0200

    feat: CanconerDia3 y cançoner tot fet

--- src/Canconer.jsx
+++ src/Canconer.jsx
@@ -2,15 +2,17 @@ import * as React from "react";
 import Typography from "@mui/material/Typography";
 import CanconerDia1 from "./CanconerDia1";
 import CanconerDia2 from "./CanconerDia2";
+import CanconerDia3 from "./CanconerDia3";
 
-export default function Canconer() {
+export default function Canconer({ day }) {
   return (
     <div>
       <Typography variant="h4" gutterBottom>
         Cançoner
       </Typography>
-      <CanconerDia1 />
-      <CanconerDia2 />
+      {day === 1 && <CanconerDia1 />}
+      {day === 2 && <CanconerDia2 />}
+      {day === 3 && <CanconerDia3 />}
     </div>
   );
 }
---
+++ src/CanconerDia3.jsx
@@ -0,0 +1,314 @@
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+
+export default function CanconerDia3() {
+  return (
+    <>
+      <Typography sx={{ color: "red" }} variant="h5" gutterBottom>
+        Per a l’oració del matí
+      </Typography>
+      <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
+        EL FRUIT DEL SILENCI
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        El fruit del silenci és l’oració
+        <br />
+        El fruit de l’oració és la fe
+        <br />
+        El fruit de la fe és l’amor
+        <br />
+        El fruit de l’amor és el servei
+        <br />
+        El fruit del servei és la pau
+        <br />
+      </Typography>
+      <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
+        L’AJUDA EM VINDRÀ DEL SENYOR{" "}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        L’ajuda em vindrà del Senyor, 
+        <br />
+        del Senyor, el nostre Déu, 
+        <br />
+        que ha fet el cel i la terra, 
+        <br />
+        el cel i la terra.
+        <br />
+      </Typography>
+      <Typography sx={{ color: "red" }} variant="h5" gutterBottom>
+        Per a la missa de la tarde{" "}
+      </Typography>
+      <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
+        GLÒRIA{" "}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Hem vingut aquí Senyor Jesús
+        <br />
+        seduïts pel teu Sant Esperit.
+        <br />
+        Confiem en la teva paraula
+        <br />
+        que ens parla a dintre el cor.
+        <br />
+        <br />
+        Prop del teu amor volem restar
+        <br />
+        sabedors que sempre ets fidel.
+        <br />
+        Deixarem que ens omplis d'alegria,
+        <br />
+        i t'obrirem el cor.
+        <br />
+        <br />
+        GLÒRIA, GLÒRIA
+        <br />
+        SEMPRE SERÀS NOSTRE SENYOR.
+        <br />
+        GLÒRIA, GLÒRIA
+        <br />
+        SEMPRE SERÀS NOSTRE SENYOR.
+        <br />
+        <br />
+        Tu transformaràs les nostre vides,
+        <br />
+        en la teva pau caminarem;
+        <br />
+        i aprendrem de Tu com és el Pare,
+        <br />
+        i Ell ens farà forts.
+        <br />
+        <br />
+        Creixerem units en l'esperança,
+        <br />
+        fes de tots nosaltres homes nous
+        <br />
+        que proclamin per tota la terra,
+        <br />
+        que tu ets el nostre Senyor.
+        <br />
+        <br />
+        GLÒRIA, GLÒRIA…
+        <br />
+      </Typography>
+      <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
+        CELEBREM {" "}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Celebrem la llum,
+        <br />
+        Celebrem la vida,
+        <br />
+        Celebrem la vida,
+        <br />
+        Celebrem.
+        <br />
+      </Typography>
+      <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
+        AMANDO HASTA EL EXTREMO{" "}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Déjame, señor, mirarte bien por dentro
+        <br />
+        Entrar en tu corazón y dejarme seducir
+        <br />
+        Y que aumenten mis deseos de querer ser como tú
+        <br />
+        Conocerte internamente, amarte y seguirte más
+        <br />
+        Apostar mi vida junto a ti, déjame verte, Señor
+        <br />
+        <br />
+        Amando  hasta el extremo
+        <br />
+        Dejándote la  piel
+        <br />
+        Entregando las entrañas
+        <br />
+        Tus entrañas de mujer
+        <br />
+        En una toalla y un lebrillo
+        <br />
+        En un acariciar los pies
+        <br />
+        En un mirarnos hasta el fondo
+        <br />
+        Sin nada que reprochar
+        <br />
+        Y  sin nada que pedir
+        <br />
+        Y con tanto para dar
+        <br />
+        <br />
+        Yo, el maestro y el señor, ya no puedo amaros más
+        <br />
+        Pues como el padre me ha amado, así os he amado yo
+        <br />
+        Os dejo mi vida entera en este vino y este pan
+        <br />
+        Este pan que soy yo mismo que me parto y que me doy
+        <br />
+        Mi deseo es que os améis de corazón
+        <br />
+        Yo también os quiero ver
+        <br />
+        <br />
+        Amando  hasta el extremo
+        <br />
+        Dejándote la  piel
+        <br />
+        Entregando las entrañas
+        <br />
+        Cómo lo hace una mujer
+        <br />
+        En una toalla y un lebrillo
+        <br />
+        En un acariciar los pies
+        <br />
+        En un mirarnos hasta el fondo
+        <br />
+        Sin nada que reprochar
+        <br />
+        Y  sin nada que pedir
+        <br />
+        Y con tanto para dar
+        <br />
+      </Typography>
+      <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
+        L’AMOR NO PASSARÀ MAI{" "}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Si jo parlés mil llengües però no estimés,
+        <br />
+        si fos capaç de veure el que vindrà,
+        <br />
+        si fos capaç de moure les muntanyes amb la fe
+        <br />
+        no serviria de res.
+        <br />
+        <br />
+        Si jo donés als pobres tot allò que tinc
+        <br />
+        o fos capaç de vendre’m per esclau,
+        <br />
+        si pretengués donar-ho tot però no estimés <br />
+        no serviria de res.
+        <br />
+        <br />
+        Valdria tant com un timbal trencat,
+        <br />
+        un esquellot esquerdat...
+        <br />
+        <br />
+        L’amor és pacient, 
+        <br />
+        l’amor és bondadós, 
+        <br />
+        l’amor troba el seu goig en la veritat,
+        <br />
+        l’amor ho excusa tot i tot ho creu. 
+        <br />
+        <br />
+        L’amor no és envejós, 
+        <br />
+        l’amor no és altiu ni orgullós, 
+        <br />
+        l’amor no és egoista ni groller, 
+        <br />
+        l’amor... no passarà mai!
+        <br />
+      </Typography>
+      <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
+        LES BENAURANCES{" "}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Benaurats aquells que avui encara confien <br />
+        Benaurats aquells que no han perdut la por.
+        <br />
+        Benaurats aquells que canten tot el dia.
+        <br />
+        Benaurats aquells que no agafen bé el to
+        <br />
+        <br />
+        <strong>
+          Benaurats seran els qui somien deixar-ho tot! <br />
+          Benaurats seran els qui treballen amb les mans. <br />
+          Benaurats seran als qui al banquet no han convidat!
+          <br />
+          Benaurats seran els qui no obliden els malalts.
+          <br />
+          <br />
+        </strong>
+        Benaurats aquells que esperen en silenci. <br />
+        Benaurats aquells que encara estan de dol.
+        <br />
+        Benaurats aquells que no han trobat la ruta.
+        <br />
+        Benaurats aquells que ploren pels demés.
+        <br />
+        <br />
+        <strong>
+          Benaurats seran els que al mar moren ofegats!
+          <br />
+          Benaurats seran els qui s’esforcen en canviar.
+          <br />
+          Benaurats seran els qui no entenen les raons!
+          <br />
+          Benaurats seran els que s’estimen de debò.
+          <br />
+          <br />
+        </strong>
+        Benaurats aquells que viuen en la fosca. <br />
+        Benaurats aquells que el rumb els porta lluny.
+        <br />
+        Benaurats aquells que avui el llop assetja.
+        <br />
+        Benaurats aquells que trenquen soledats.
+        <br />
+        <br />
+        <strong>
+          Benaurats seran els que caminen sense alè!
+          <br />
+          Benaurats seran els qui confien en la gent.
+          <br />
+          Benaurats seran els qui perdonen sense fe!
+          <br />
+          Benaurats seran els que al dimoni han desarmat.
+          <br />
+          <br />
+        </strong>
+        Benaurats aquells que saben les respostes.
+        <br />
+        Benaurats aquells que vetllen pels petits.
+        <br />
+        Benaurats aquells que no han tingut infància.
+        <br />
+        Benaurats aquells que escolten tots els crits.
+        <br />
+        <br />
+        <strong>
+          …els que treballen amb les mans.
+          <br />
+          … els qui no obliden els malalts.
+          <br />
+          … els que s’esforcen per canviar.
+          <br />
+          … els que s’estimen de debò.
+          <br />
+          … els que confien en la gent.
+          <br />
+          … els que al dimoni han desarmat.
+          <br />
+        </strong>
+      </Typography>
+
+      <Typography variant="h6" sx={{ fontWeight: "bold" }} gutterBottom>
+        MAGNIFICAT{" "}
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Magnificat, Magnificat <br />
+        Magnificat anima mea Dominum (2)
+        <br />
+      </Typography>
+    </>
+  );
+}
